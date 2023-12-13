6a89a725eaa9f72e333f47644faae7d19876295f
AntipasG <antipasbelihu@gmail.com>
2023-12-13 03:38:40 +0300

    adding the shell.h header of all

--- shell.h
+++ shell.h
@@ -1,12 +1,20 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <string.h>
 
+void shell_prompt(void);
+char *read_command(void);
+void execute_command(char *command);
+void execute_external_command(char *command);
+void execute_builtin_exit(void);
+void execute_builtin_env(void);
 
+
+extern char **environ;
 #endif
